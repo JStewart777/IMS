3d14dddbdf5eabe7746cf6ff31629be4a4eed316
anjalayadavalli <45400720+anjalayadavalli@users.noreply.github.com>
2018-11-28 14:23:55 -0500

    Update GroceryStore.java

--- GroceryStore.java
+++ GroceryStore.java
@@ -1,7 +1,8 @@
-package InventorySys;
-
+import java.util.*;
 public class GroceryStore {
 	//TODO: make a collection to hold this data
+	
+	HashMap<Item,Integer> Store = new HashMap<Item,Integer>();
 	
 	public void putOnSale() {
 		//TODO: select either meat or produce, 
