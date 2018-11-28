c2d21beac135f1ccbfd3a5b94ab06577830dfe85
anjalayadavalli <45400720+anjalayadavalli@users.noreply.github.com>
2018-11-28 17:44:06 -0500

    Update Membership.java

--- Membership.java
+++ Membership.java
@@ -1,4 +1,42 @@
-
+import java.util.*;
 public class Membership {
+	private int memID;
+	private String memName;
+	private static int memCount = 0;
+	
+	
+	public Membership()   {
+		memID = memCount;
+		memName = "TBD";
+		memCount++;
+	}
+	
+	public Membership(String name)   {
+		memID = memCount;
+		memName = name;
+	}
+	
+	
+	public int getID()   {
+		return memID;
+	}
+	
+	public void setID(int id)   {
+		memID = id;
+	}
+	
+	public String getName()   {
+		return memName;
+	}
+	
+	public void setName(String name)   {
+		memName = name;
+	}
+	
+	
+	public void print()   {
+		System.out.println("Name: " + memName);
+		System.out.println("ID Number: " + memID);
+	}
 
 }
