d939ab1eee5fc1a537974503a4a620dfd0146017
anjalayadavalli <45400720+anjalayadavalli@users.noreply.github.com>
2018-11-28 17:49:54 -0500

    Update GroceryStore.java

--- GroceryStore.java
+++ GroceryStore.java
@@ -1,24 +1,44 @@
-
 import java.util.*;
 public class GroceryStore {
-	public HashSet<Item> StoreCatalog = new HashSet<Item>();
+	//TODO: make a collection to hold this data
 	
-	public void addProduce(String description, double price, boolean byPound) {
-		Produce p = new Produce(description, price, byPound); 
-		StoreCatalog.add(p);
+	HashMap<Item,Integer> Store = new HashMap<Item,Integer>();
+	ArrayList<Membership> MemList = new ArrayList<Membership>();
+	
+	
+	public void putOnSale() {
+		//TODO: select either meat or produce, 
+		//and input % to discount the item with less than x days till expiration
 	}
 	
-	public void addMeat(String description, double price, boolean byPound, String animal) {
-		Meat m = new Meat(description, price, byPound, animal);
-		StoreCatalog.add(m);
-	}
-	
-	public void printCatalogItems() {
-		for (Item i : StoreCatalog) {
-			i.printDesc();
+	public void addMember(Scanner scnr)   {
+		System.out.println("Enter name:");
+		String name = scnr.nextLine();
+		scnr.nextLine();
+		System.out.println("Did they pay the $50 membership fee? Enter 'Yes' or 'No'"); 
+		String answer = scnr.next();
+		while(answer.equalsIgnoreCase("No"))   {
+			System.out.println("You cannot obtain a membership until you have paid the $50 membership fee. Please try again.");
+			answer = scnr.next();
+		}
+		if(answer.equalsIgnoreCase("Yes"))   {
+			Membership m1 = new Membership(name);
+			MemList.add(m1);
+		}
+		else  {
+			System.out.println("Please enter either 'Yes' or 'No'");
 		}
 	}
-
+	
+	public void removeMember(int id)   {//THIS MAY OR MAY NOT WORK
+		for(Membership i : MemList)   {
+			if(i.getID() == id)   {
+				MemList.remove(i);
+			}
+		}
+	}
+	
+	
 	
 	
 }
