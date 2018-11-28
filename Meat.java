7f1e93ecae0d7913e8035c73a923661b0e0fd6a5
Sam W <44239269+CSamWhiteman@users.noreply.github.com>
2018-11-28 16:05:27 -0500

    Update Meat.java

--- Meat.java
+++ Meat.java
@@ -3,12 +3,33 @@ package InventorySys;
 public class Meat extends Item{
 	
 	String animal;
-	int daysToExpiry;
 	
+	
+	public Meat() {
+		itemId = itemCount;
+		itemDescription = "Sirloin";
+		itemPrice = 8.99;
+		pricePer = PricingCategory.POUND;
+		animal = "cow";
+		itemCount++;
+	}
+	
+	public Meat(String description, double price, boolean byPound, String animal) {
+		itemId = itemCount;
+		itemDescription = description;
+		itemPrice = price;
+		if (byPound) 
+			pricePer = PricingCategory.POUND;
+		else
+			pricePer = PricingCategory.UNIT;
+		this.animal = animal;
+		itemCount++;
+	}
 	
 	@Override
 	public void printItem() {
-		// TODO Auto-generated method stub
+		super.printItem();
+		System.out.println("Source Animal: " + animal);
 		
 	}
 	
