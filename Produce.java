570124f6cd78a1ba1dbf93e5496b19a8772859f7
Sam W <44239269+CSamWhiteman@users.noreply.github.com>
2018-11-28 16:06:11 -0500

    Update Produce.java

--- Produce.java
+++ Produce.java
@@ -1,44 +1,33 @@
 
+package InventorySys;
+
 public class Produce extends Item {
 	
-	int daysToExpiry;
-	enum Season {SPRING, SUMMER, FALL, WINTER};
-	String region;
-	Season harvestSeason;
+	
 	
 	public Produce() {
 		itemId = itemCount;
 		itemDescription = "Tomato";
 		itemPrice = 3.99;
 		pricePer = PricingCategory.POUND;
-		vendorName = "AJAX Vegetables";
-		daysToExpiry = 7;
-		harvestSeason = Season.SUMMER;
-		region = "Southeast US";
 		itemCount++;
 	}
 	
-	public int getDaysToExpiry() {
-		return daysToExpiry;
-	}
-	public void setDaysToExpiry(int daysToExpiry) {
-		this.daysToExpiry = daysToExpiry;
-	}
-	public String getRegion() {
-		return region;
-	}
-	public void setRegion(String region) {
-		this.region = region;
+	public Produce(String description, double price, boolean byPound) {
+		itemId = itemCount;
+		itemDescription = description;
+		itemPrice = price;
+		if (byPound) 
+			pricePer = PricingCategory.POUND;
+		else
+			pricePer = PricingCategory.UNIT;
+		itemCount++;
 	}
 	
-	@Override
+	
+
 	public void printItem() {
 		super.printItem();
-		System.out.println(daysToExpiry + " days until expiration.");
-		System.out.println("Region: " + region + "Harvest Season: " + harvestSeason);
-		
-		
-		
 		}
 	
 	
